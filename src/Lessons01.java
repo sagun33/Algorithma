f323cec3ae6a6a2ec50e05e5078f19226c9a020b
sagun33 <salihguenay33@gmail.com>
2022-12-05 08:33:48 +0100

    m

---
+++ src/Lessons01.java
@@ -0,0 +1,12 @@
+
+
+public class Ders1 {
+    public static void main(String[] args) {
+        int a= (int) (Math.random()*10+1);
+        if (a<5){
+            System.out.println("a 5 den kucuktur " +a);
+        }
+    }
+}
+
+
